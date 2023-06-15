1f3fe17ff7ac5c6ae3a51b0806185a1f17deb4af
cretihoy <sergzhio238@gmail.com>
2023-06-15 17:12:17 +0300

    add question factory to presenter

--- app/src/main/java/com/example/testtaskquestions/gameScreen/GamePresenter.kt
+++ app/src/main/java/com/example/testtaskquestions/gameScreen/GamePresenter.kt
@@ -1,10 +1,15 @@
 package com.example.testtaskquestions.gameScreen
 
+import com.example.testtaskquestions.QuestionFactory
 import moxy.InjectViewState
 import moxy.MvpPresenter
 import javax.inject.Inject
 
 @InjectViewState
 class GamePresenter
-@Inject constructor() : MvpPresenter<GameView>() {
+@Inject constructor(
+   private val questionFactory: QuestionFactory
+) : MvpPresenter<GameView>() {
+
+
 }
