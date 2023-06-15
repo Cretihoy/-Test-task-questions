213a5bca0e0915007a61c7dec18becd931cf2535
cretihoy <sergzhio238@gmail.com>
2023-06-15 17:11:15 +0300

    add fields to activity

--- app/src/main/java/com/example/testtaskquestions/gameScreen/GameActivity.kt
+++ app/src/main/java/com/example/testtaskquestions/gameScreen/GameActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testtaskquestions.gameScreen
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.testtaskquestions.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +12,11 @@ import javax.inject.Provider
 
 @AndroidEntryPoint
 class GameActivity : MvpAppCompatActivity(), GameView {
+
+    val questionText: TextView by lazy { findViewById(R.id.text_activity_game) }
+    val firstBookImage: ImageView by lazy { findViewById(R.id.book_1_activity_game) }
+    val secondBookImage: ImageView by lazy { findViewById(R.id.book_2_activity_game) }
+    val thirdBookImage: ImageView by lazy { findViewById(R.id.book_3_activity_game) }
 
     @Inject
     lateinit var presenterProvider: Provider<GamePresenter>
