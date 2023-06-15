18783945a98a6297a82c5596a912b68916963b1c
cretihoy <sergzhio238@gmail.com>
2023-06-15 16:39:34 +0300

    create and add components in book factory

---
+++ app/src/main/java/com/example/testtaskquestions/BookFactory.kt
@@ -0,0 +1,18 @@
+package com.example.testtaskquestions
+
+class BookFactory {
+    fun getBookList(): List<BookModel> {
+        return listOf(
+            BookModel(R.drawable.book_1, R.string.harry_potter),
+            BookModel(R.drawable.book_2, R.string.alice_adventure),
+            BookModel(R.drawable.book_3, R.string.gulliver_travels),
+            BookModel(R.drawable.book_4, R.string.king_arthur_sword),
+            BookModel(R.drawable.book_5, R.string.witcher),
+            BookModel(R.drawable.book_6, R.string.game_of_thrones),
+            BookModel(R.drawable.book_7, R.string.twilight),
+            BookModel(R.drawable.book_8, R.string.dark_tower),
+            BookModel(R.drawable.book_9, R.string.walking_castle),
+            BookModel(R.drawable.book_10, R.string.the_chronicles_of_narnia),
+        )
+    }
+}
