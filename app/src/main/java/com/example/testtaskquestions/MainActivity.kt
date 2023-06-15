a188d810439c466feca8195fa57f348af02acd6e
cretihoy <sergzhio238@gmail.com>
2023-06-15 14:31:47 +0300

    add presenter in main activity

--- app/src/main/java/com/example/testtaskquestions/MainActivity.kt
+++ app/src/main/java/com/example/testtaskquestions/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.example.testtaskquestions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import moxy.MvpAppCompatActivity
+import moxy.ktx.moxyPresenter
+import javax.inject.Inject
+import javax.inject.Provider
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : MvpAppCompatActivity(), MainView {
+
+    @Inject
+    lateinit var presenterProvider: Provider<MainPresenter>
+    private val presenter by moxyPresenter { presenterProvider.get() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
